cdbc535cb5e8c18e4469b4b3720b4972e8eb2ecb
Stefanny <stefannypdao@gmail.com>
2025-03-02 00:26:53 -0300

    fix: header background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Faq from '@/components/Faq/Faq';
 import styles from './page.module.css';
+import Header from '@/components/Header/Header';
 
 export default function Home() {
   return (
     <>
     <div className={styles.container}>
+      <Header />
       <h1 className={styles.title}>Seja bem-vindo ao site SEMCOMP 2025</h1>
     </div>
     <Faq />
