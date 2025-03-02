2f10b6576563ee92e575625c99d0d7e01dc0f487
Stefanny <stefannypdao@gmail.com>
2025-03-01 22:08:54 -0300

    feat: component footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,42 @@
+'use client'
+import styles from './footer.module.css';
+import Image from 'next/image';
+import LogoInfo from '../../../public/logos/logo info.png'
+import LogoIC from '../../../public/logos/logo ic.png'
+import LogoSemcomp from '../../../public/logos/logo semcomp.png'
+
 export default function Footer() {
     return (
-      <></>
+      <div className={styles.footerContainer}>
+        <div className={styles.footerEmpresas}>
+          <div>
+            <h3>Realização:</h3>
+            <Image src={LogoInfo} alt={'Logo InfoJr'} width={131}/>
+          </div>
+
+          <div>
+            <h3>Apoio:</h3>
+            <Image src={LogoIC} alt={'Logo Instituto de Computação'} width={150}/>
+          </div>
+        </div>
+
+        <div className={styles.footerTexto}>
+          <p>
+            A SEMCOMP SSA (Semana de Computação de Salvador) é um evento anual realizado na UFBA pela InfoJr UFBA. Seu propósito é unir e fortalecer a comunidade acadêmica e entusiastas em tecnologia, inovação e empreendedorismo.
+          </p>
+        </div>
+
+      
+        <div className={styles.footerProgramacao}>
+          <Image src={LogoSemcomp} alt={'Logo SEMCOMP 2025'} width={181}/>
+
+          <ul>
+            <li>Programação</li>
+            <li>FAQ</li>
+            <li>Hackathon</li>
+            <li>Contato</li>
+          </ul>
+        </div>
+      </div>
     );
   }
