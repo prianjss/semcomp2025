5d4d5b6095c0f021aa632d5921c6a9a183e6f62e
Priscila Anjos <priscilaanjos@infojr.com.br>
2025-03-02 11:55:20 -0300

    🐛 fix: remove item no componente do dia 4

--- src/components/DiaQuatro/DiaQuatro.tsx
+++ src/components/DiaQuatro/DiaQuatro.tsx
@@ -18,9 +18,6 @@ export default function DiaQuatro() {
                         <h1 className={styles.titulo}>12:00 - <span>ABERTURA</span></h1>
                         <p className={styles.sobre}>A SEMCOMP SSA (Semana de Computação de Salvador) é um evento anual realizado na UFBA pela InfoJr UFBA. Seu propósito é unir e fortalecer a comunidade acadêmica e entusiastas em tecnologia, inovação e empreendedorismo.
                         Buscamos criar um ambiente acolhedor e acessível, onde todos os participantes se sintam bem-vindos e representados.</p>
-                        <h3 className={styles.palestranteP}>Palestrante</h3>
-                        <p className={styles.cargoP}>Former CEO da Meta</p>
-                        <p className={styles.nomeP}>Mark Zuckerberg</p>
                     </div>
                     <div className={styles.cardImage}>
                         <img src="./hackathon/hack1.png" alt="Hackathon SemComp 2024" className={styles.imagem} />
