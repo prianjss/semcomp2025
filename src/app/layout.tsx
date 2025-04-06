6c43d59c4a7cbd48a1f8678d14bc9c63cc441862
Bruno-Emanoel <brunoemanoel.l.x@gmail.com>
2025-04-06 20:50:59 -0300

    :Mobile Phone: Responsive: Fixed visualization for screens between 350 and 500px

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=IBM+Plex+Sans:wght@400;500;700&display=swap"
           rel="stylesheet"
         />
-        <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
       </head>
       <body cz-shortcut-listen="true">
         {children}
