e29585d6c9d18d0008aa08aba9cb364a8aecbccc
João Victor <jv.joaocoelho@gmail.com>
2025-08-11 11:22:06 -0300

    feat: update event times to 00:00 and adjust text formatting in DiaQuatro component

--- src/components/DiaQuatro/DiaQuatro.tsx
+++ src/components/DiaQuatro/DiaQuatro.tsx
@@ -8,17 +8,17 @@ export default function DiaQuatro() {
 
                 <div className={styles.card}>
                     <div className={styles.conteudoCard}>
-                        <h1 className={styles.titulo}>12:00 - <span>ABERTURA</span></h1>
+                        <h1 className={styles.titulo}>00:00 - <span>ABERTURA</span></h1>
                         <p className={styles.sobre}>A SEMCOMP SSA (Semana de Computação de Salvador) é um evento anual realizado na UFBA pela InfoJr UFBA. Seu propósito é unir e fortalecer a comunidade acadêmica e entusiastas em tecnologia, inovação e empreendedorismo.
-                        Buscamos criar um ambiente acolhedor e acessível, onde todos os participantes se sintam bem-vindos e representados.</p>
+                            Buscamos criar um ambiente acolhedor e acessível, onde todos os participantes se sintam bem-vindos e representados.</p>
                     </div>
                 </div>
 
                 <div className={styles.card}>
                     <div className={styles.conteudoCard}>
-                        <h1 className={styles.titulo}>13:00 - <span>ABERTURA</span></h1>
+                        <h1 className={styles.titulo}>00:00 - <span>ABERTURA</span></h1>
                         <p className={styles.sobre}>A SEMCOMP SSA (Semana de Computação de Salvador) é um evento anual realizado na UFBA pela InfoJr UFBA. Seu propósito é unir e fortalecer a comunidade acadêmica e entusiastas em tecnologia, inovação e empreendedorismo.
-                        Buscamos criar um ambiente acolhedor e acessível, onde todos os participantes se sintam bem-vindos e representados.</p>
+                            Buscamos criar um ambiente acolhedor e acessível, onde todos os participantes se sintam bem-vindos e representados.</p>
                     </div>
                     <div className={styles.cardImage}>
                         <Image
@@ -37,26 +37,26 @@ export default function DiaQuatro() {
 
                 <div className={styles.card}>
                     <div className={styles.conteudoCard}>
-                        <h1 className={styles.titulo}>14:00 - <span>ABERTURA</span></h1>
+                        <h1 className={styles.titulo}>00:00 - <span>ABERTURA</span></h1>
                         <p className={styles.sobre}>A SEMCOMP SSA (Semana de Computação de Salvador) é um evento anual realizado na UFBA pela InfoJr UFBA. Seu propósito é unir e fortalecer a comunidade acadêmica e entusiastas em tecnologia, inovação e empreendedorismo.
-                        Buscamos criar um ambiente acolhedor e acessível, onde todos os participantes se sintam bem-vindos e representados.</p>
+                            Buscamos criar um ambiente acolhedor e acessível, onde todos os participantes se sintam bem-vindos e representados.</p>
                     </div>
                     <div className={styles.cardImage}>
                         <Image
                             src="/hackathon/hack2.png"
                             alt="Hackathon SemComp 2024"
                             className={styles.imagemHack}
-                            width={396} 
-                            height={300} 
+                            width={396}
+                            height={300}
                         />
                     </div>
                 </div>
 
                 <div className={styles.card}>
                     <div className={styles.conteudoCard}>
-                        <h1 className={styles.titulo}>17:40 - <span>PREMIAÇÃO</span></h1>
+                        <h1 className={styles.titulo}>00:00 - <span>PREMIAÇÃO</span></h1>
                         <p className={styles.sobre}>A SEMCOMP SSA (Semana de Computação de Salvador) é um evento anual realizado na UFBA pela InfoJr UFBA. Seu propósito é unir e fortalecer a comunidade acadêmica e entusiastas em tecnologia, inovação e empreendedorismo.
-                        Buscamos criar um ambiente acolhedor e acessível, onde todos os participantes se sintam bem-vindos e representados.</p>
+                            Buscamos criar um ambiente acolhedor e acessível, onde todos os participantes se sintam bem-vindos e representados.</p>
                     </div>
                 </div>
 
